4c18e44e7d8ffd9e90a958b582f980f1bfaa9fa3
lesthy900 <lesthy900@gmail.com>
2023-04-28 01:42:25 +0200

    ejercicios jss

---
+++ asset/js/les.js
@@ -0,0 +1,61 @@
+
+
+let miPrimerNombre =  "lesty"
+let miPrimerApellido =  "valencia"
+let miSegundoApellido =  "parra"
+let edadLesty = 25
+let edadMascota =6
+let miNombreComoleto = "lesty valencia parra"
+let variablesVistas = "var let number const undefine null boolean"
+
+let nombreDeMiMascota = "zues"
+
+
+console.log ( "lesty ",  typeof "miPrimerNombre")
+console.log (miPrimerApellido)
+console.log ( miSegundoApellido)
+console.log (nombreDeMiMascota)
+console.log (typeof edadLesty)
+console.log (edadMascota)
+console.log (miNombreComoleto)
+console.log (variablesVistas)
+
+
+const resultadoSuma = edadLesty + edadMascota
+console.log (resultadoSuma)
+
+const resultadoResta = edadLesty - edadMascota
+console.log (resultadoResta)
+
+const resutadoMultiplicacion = edadLesty * edadMascota
+console.log (resutadoMultiplicacion)
+
+const resultadoDivision = edadLesty / edadMascota
+console.log (resultadoDivision)
+
+
+const resutadoResto = edadLesty % edadMascota
+console.log (resutadoResto)
+
+const resutadoExponete = edadLesty ** edadMascota
+console.log (resutadoExponete)
+
+let nombre = "lesty juan"
+let apellido = "valencia"
+const nombreCompleto = `${nombre} ${apellido}`
+
+/* 
+const ejemplos = "lesty  \n \"parra\" "
+console.log( ejemplos)
+ */
+
+console.log (20==20)
+console.log (20=="20")
+console.log (20==="20")
+console.log (20 != 20)
+console.log (20 !==20)
+console.log (20 !=="20")
+console.log (20 > 10)
+console.log (10<20)
+console.log (10 >= "20")
+console.log (10 >= "10")
---
+++ asset/js/les2.js
@@ -0,0 +1,31 @@
+/* 
+console.log("hola ")
+console.log(2+1)
+
+
+function imprimirSaludo(){
+    console.log("hola")
+}
+
+
+imprimirSaludo()
+
+  
+/* funsion anonima */
+
+let suma= function(){
+    console.log(2+1)
+}
+
+suma()
+
+
+/*  */
+
+function sumar(){
+    let y = 1
+    console.log(x)
+    let x =2
+    console.log(y)
+    console.log(x+y)
+}
